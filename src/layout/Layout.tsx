f887f8a9ea284724dff7207a8d405e3c81bd8e5b
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-03-02 10:34:19 -0600

    delete: code colors

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,3 @@
-import CodigoColores from '../components/CodigoColores'
-import Conectado from '../components/Conectado'
-
 interface Props {
   children?: React.ReactNode
 }
@@ -11,11 +8,6 @@ const DashboardLayout = ({ children }: Props) => {
       <main className="flex-1 px-3 sm:px-6 py-4 sm:py-6 space-y-6">
         {children}
       </main>
-
-      <footer className="flex justify-between border-t border-border px-4 sm:px-6 py-3">
-        <CodigoColores />
-        <Conectado />
-      </footer>
     </div>
   )
 }
