3a36defea773ba8f8f582a2589cc5cd401142cde
keincarrilloR <kein.carrillo@conduit.com.mx>
2026-03-19 08:42:27 -0600

    fix: add HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import MaquinaPage from './pages/MaquinaPage'
 
 const App = () => {
   return (
-    <BrowserRouter basename="/operaweb/dist/">
+    <HashRouter basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/maquina/:id" element={<MaquinaPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
